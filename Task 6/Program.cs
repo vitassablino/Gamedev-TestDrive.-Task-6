0155a3cf756e04a4f7926c0629a24a39a1575eb3
vitassablino <105976325+vitassablino@users.noreply.github.com>
2022-05-22 15:00:14 +0300

    Добавьте файлы проекта.

---
+++ Task 6/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        uint colsAmount = 3;
+        uint picturesAmount = 52;
+        uint filledPicturesRows;
+        uint picturesHidden;
+
+        filledPicturesRows = picturesAmount / colsAmount;
+        picturesHidden = picturesAmount % colsAmount;
+        Console.WriteLine($"Заполнено рядов - {filledPicturesRows}");
+        Console.WriteLine($"Не показано картинок - {picturesHidden}");
+    }
+}
